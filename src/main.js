dad2ed8c7b19e2766af5a3a55e083fde3551361d
Kbalod <kbalod@yandex.ru>
2022-04-27 22:30:30 +0300

    Задание 3

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import MainFilterView from './view/filters-menu-main.js';
 import SortFilterView from './view/filters-menu-sort.js';
 import RankUserView from './view/rank-user.js';
 import FilmsPresenter from './presenter/presenter-films.js';
+import PopupFilmView from './view/popup.js';
 import {render} from './render.js';
 
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = document.querySelector('.header');
+const siteFooterElement = document.querySelector('.footer');
 const filmsPresenter = new FilmsPresenter();
 
 render(new MainFilterView(), siteMainElement);
@@ -13,3 +15,4 @@ render(new SortFilterView(), siteMainElement);
 render(new RankUserView(), siteHeaderElement);
 
 filmsPresenter.init(siteMainElement);
+render(new PopupFilmView(), siteFooterElement);
